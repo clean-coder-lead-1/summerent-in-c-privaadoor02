7f132d3e80541344b07840d1b8b1ef1e49c55e0f
Prince Varghese (MS/ECF4-XC) <prince.varghese@in.bosch.com>
2022-03-28 19:06:55 +0530

    Upadted Test cases and Readme.md

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,96 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
+/**
+ * @brief Unit Test case for inferBreach
+ * 
+ */
+
+
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+}
+
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(45, 20, 30) == TOO_HIGH);
+}
+
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
+
+
+/**
+ * @brief Unit Test case for classifyTemperatureBreach
+ * 
+ */
+TEST_CASE("Classify Temperature Breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 30) == NORMAL);
+}
+
+TEST_CASE("Classify Temperature Breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 39) == NORMAL);
+}
+
+TEST_CASE("Classify Temperature Breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 43) == NORMAL);
+}
+
+/**
+ * @brief Unit Test case for checkAndAlert
+ * 
+ */
+
+TEST_CASE("Check the temperature And Alert to controller") {
+
+  AlertTarget target;
+  BatteryCharacter batchar;
+
+  batchar.coolingType = MED_ACTIVE_COOLING;
+  target=TO_CONTROLLER;
+  REQUIRE(target==TO_CONTROLLER);
+  checkAndAlert(target, batchar, 45);
+}
+
+TEST_CASE("Check the temperature And Alert via Mail") {
+
+  AlertTarget target;
+  BatteryCharacter batchar;
+
+  batchar.coolingType = MED_ACTIVE_COOLING;
+  target=TO_EMAIL;
+  REQUIRE(target==TO_EMAIL);
+  checkAndAlert(target, batchar, 50);
+}
+
+/**
+ * @brief Unit Test case for sendToController
+ * 
+ */
+TEST_CASE(" Sent to controller check") {
+
+  BreachType Btype=TOO_HIGH;
+ 
+  REQUIRE(Btype==TOO_HIGH);
+
+  sendToController(TOO_HIGH);
+}
+
+/**
+ * @brief Unit Test case for sendToEmail
+ * 
+ */
+
+TEST_CASE(" Sent to Email check") {
+
+  BreachType Btype=TOO_HIGH;
+ 
+  REQUIRE(Btype==TOO_HIGH);
+  sendToEmail(TOO_HIGH);
+
+  REQUIRE(Btype==TOO_LOW);
+  sendToEmail(TOO_LOW);
+
+  REQUIRE(Btype==NORMAL);
+  sendToEmail(NORMAL);
 }
