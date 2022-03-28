7218d07ea451dd3bc08dc527abcd38acc516884f
Prince Varghese (MS/ECF4-XC) <prince.varghese@in.bosch.com>
2022-03-28 19:21:30 +0530

    Corrected Testcase defenitions

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -9,15 +9,15 @@
  */
 
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to limits TOO_LOW") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to limits TOO_HIGH") {
   REQUIRE(inferBreach(45, 20, 30) == TOO_HIGH);
 }
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to limits NORMAL") {
   REQUIRE(inferBreach(25, 20, 30) == NORMAL);
 }
 
@@ -26,15 +26,15 @@ TEST_CASE("infers the breach according to limits") {
  * @brief Unit Test case for classifyTemperatureBreach
  * 
  */
-TEST_CASE("Classify Temperature Breach according to limits") {
+TEST_CASE("Classify Temperature Breach according to Cooling Type PASSIVE_COOLING") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 30) == NORMAL);
 }
 
-TEST_CASE("Classify Temperature Breach according to limits") {
+TEST_CASE("Classify Temperature Breach according to Cooling Type HI_ACTIVE_COOLING") {
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 39) == NORMAL);
 }
 
-TEST_CASE("Classify Temperature Breach according to limits") {
+TEST_CASE("Classify Temperature Breach according to Cooling Type MED_ACTIVE_COOLING") {
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 43) == NORMAL);
 }
 
